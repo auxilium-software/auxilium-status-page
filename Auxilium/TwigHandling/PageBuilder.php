1f4e51be80e2795c52fa4a72f7ffc423f95dd08f
Cerys <cerysamberlewis@gmail.com>
2026-07-23 16:14:32 +0100

    Update PageBuilder.php

--- Auxilium/TwigHandling/PageBuilder.php
+++ Auxilium/TwigHandling/PageBuilder.php
@@ -39,56 +39,11 @@ final class PageBuilder
     }
 
     #[NoReturn]
-    public static function AutoRender(array $variables = []): void
-    {
-        self::Render(
-            template: self::guessTargetTwigFile(),
-            variables: $variables,
-            useAuth: true
-        );
-    }
-
-    #[NoReturn]
     public static function Render(string $template, array $variables = [], bool $useAuth = true): void
     {
         $builder = new self($useAuth);
         echo $builder->twig->render($template, $variables);
         exit();
-    }
-
-    /**
-     * Determines the Twig template path based on the current REQUEST_URI.
-     *
-     * @throws \RuntimeException If the template file does not exist.
-     */
-    private static function guessTargetTwigFile(): string
-    {
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $uri = preg_replace('/[^a-zA-Z0-9\/_\-.]/', '', $uri); // Sanitize
-
-        $twigFile = str_replace('.php', '.html.twig', $uri);
-
-        if (!str_ends_with($twigFile, '.html.twig')) {
-            $twigFile .= 'index.html.twig';
-        }
-
-        $fullPath = __DIR__ . '/../../Templates/Pages' . $twigFile;
-
-        if (!file_exists($fullPath)) {
-            throw new \RuntimeException("Template not found: {$twigFile}");
-        }
-
-        return 'Pages' . $twigFile;
-    }
-
-    #[NoReturn]
-    public static function AutoRenderUnsafe(array $variables = []): void
-    {
-        self::Render(
-            template: self::guessTargetTwigFile(),
-            variables: $variables,
-            useAuth: false
-        );
     }
 
     #[NoReturn]
