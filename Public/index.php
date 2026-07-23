da77df21c4fda7e62a28ce372a96994cfa246e17
Cerys <cerysamberlewis@gmail.com>
2026-07-23 16:14:31 +0100

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -5,7 +5,9 @@ use Auxilium\TwigHandling\PageBuilder;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    PageBuilder::AutoRender();
+    PageBuilder::Render(
+        template: "/Pages/index.html.twig"
+    );
 } catch (Exception $e) {
     PageBuilder::RenderInternalSystemError($e);
 }
